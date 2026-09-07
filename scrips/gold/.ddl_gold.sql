ebaaa4dabf019c7e26f3a6342a24ac772ce375bf
Abdul Manan Yousaf <abdulmanan.202400417@gcuf.edu.pk>
2026-09-07 12:33:48 +0500

    Update .ddl_gold.sql

--- scrips/gold/.ddl_gold.sql
+++ scrips/gold/.ddl_gold.sql
@@ -1,1 +1,35 @@
 
+                    ┌───────────────────┐
+                    │  dim_customers    │
+                    │                   │
+                    │ customer_sk       │
+                    │ customer_id       │
+                    │ name              │
+                    │ country           │
+                    └─────────┬─────────┘
+                              │
+                              │
+                              ▼
+                    ┌───────────────────┐
+                    │    fact_sales     │
+                    │                   │
+                    │ order_number      │
+                    │ customer_sk       │
+                    │ product_key       │
+                    │ order_date        │
+                    │ quantity          │
+                    │ price             │
+                    │ sales             │
+                    └─────────┬─────────┘
+                              │
+                              │
+                              ▼
+                    ┌───────────────────┐
+                    │  dim_products     │
+                    │                   │
+                    │ product_key       │
+                    │ product_id        │
+                    │ product_name      │
+                    │ category          │
+                    │ subcategory       │
+                    └───────────────────┘
